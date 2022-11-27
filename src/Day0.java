f6242aad7ff3a2dabe01265b9d749e3d47f12a0d
Antonina Tellow <antoninatellow@peters-mbp.home>
2022-11-27 17:01:41 +0100

    Day 0

---
+++ src/Day0.java
@@ -0,0 +1,19 @@
+public class Day0 {
+    public static void part1() {
+        var input = Reader.lines(0);
+        var sum = 0;
+        for (String number : input) {
+            sum += Integer.parseInt(number);
+        }
+        System.out.println("The sum of numbers in 0.txt is: " + sum);
+    }
+
+    public static void part2() {
+        var input = Reader.lines(0);
+        var product = 1;
+        for (String number : input) {
+            product *= Integer.parseInt(number);
+        }
+        System.out.println("The product of numbers in 0.txt is: " + product);
+    }
+}
---
+++ src/Reader.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Reader {
+    public static List<String> lines(int day) {
+        try {
+            return Files.readAllLines(Path.of("input/" + day + ".txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
