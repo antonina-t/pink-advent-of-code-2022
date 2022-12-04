fd4d35f67a4c41bec4fca200e0c1c784a178fd7b
Antonina Tellow <antoninatellow@Peters-MacBook-Pro.local>
2022-12-04 15:36:19 +0100

    Day 2 pt 2

--- src/Day2.java
+++ src/Day2.java
@@ -21,5 +21,19 @@ public class Day2 {
 
     public static void part2() {
         var input = Reader.lines("2");
+
+        var map = new HashMap<String, Integer>();
+        map.put("A X", 0 + 3);
+        map.put("A Y", 3 + 1);
+        map.put("A Z", 6 + 2);
+        map.put("B X", 0 + 1);
+        map.put("B Y", 3 + 2);
+        map.put("B Z", 6 + 3);
+        map.put("C X", 0 + 2);
+        map.put("C Y", 3 + 3);
+        map.put("C Z", 6 + 1);
+
+        var result = input.stream().map(map::get).reduce(Integer::sum).orElseThrow();
+        System.out.println(result);
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Let's save Christmas!");
         Day2.part1();
+        Day2.part2();
     }
 }
